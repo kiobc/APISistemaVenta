6ae1bfbb99ebbc5a8aa85448fd83630bc36b6d18
kiobc <brayan.calderon3464@utc.edu.ec>
2024-04-17 17:28:30 -0500

    Agregada clases faltantes y desarrollado parte del automapper

---
+++ SistemasVentas.Utility/AutoMapperProfile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SistemaVentas.DTO;
+using SistemaVentas.Model;
+
+namespace SistemasVentas.Utility
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            #region Rol
+            CreateMap<Rol, RolDTO>().ReverseMap();
+            #endregion Rol
+
+            #region Menu
+            CreateMap<Menu, MenuDTO>().ReverseMap();
+            #endregion Menu
+
+            #region Usuario
+            CreateMap<Usuario, UsuarioDTO>()
+            .ForMember(destino =>
+             destino.RolDescripcion,
+             opt => opt.MapFrom(origen => origen.IdRolNavigation.Nombre))
+            .ForMember(destino =>
+             destino.EsActivo,
+             opt => opt.MapFrom(origen => origen.EsActivo == true ? 1 : 0));
+
+            CreateMap<Usuario, SesionDTO>()
+            .ForMember(destino =>
+             destino.RolDescripcion,
+             opt => opt.MapFrom(origen => origen.IdRolNavigation.Nombre));
+
+            CreateMap<UsuarioDTO, Usuario>()
+            .ForMember(destino =>
+             destino.IdRolNavigation,
+             opt => opt.Ignore())
+            .ForMember(destino =>
+             destino.EsActivo,
+             opt => opt.MapFrom(origen => origen.EsActivo == 1 ? true : false));
+            #endregion Usuario
+
+
+
+        }
+    }
+}
