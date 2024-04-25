9fb131da0e65894e40c1d0dc086d56b2ace3cc05
kiobc <brayan.calderon3464@utc.edu.ec>
2024-04-25 12:24:36 -0500

    Creados las interfaces v1

---
+++ SistemaVentas.BLL/Servicios/UsuarioService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SistemaVentas.BLL.Servicios.Contrato;
+using SistemaVentas.DAL.Repositorios.Contrato;
+using SistemaVentas.DTO;
+using SistemaVentas.Model;
+
+namespace SistemaVentas.BLL.Servicios
+{
+    public class UsuarioService:IUsuarioService
+    {
+        private readonly IGenericRepository<Usuario> _usuarioRepositorio;
+        private readonly IMapper _mapper;
+
+        public UsuarioService(IGenericRepository<Usuario> usuarioRepositorio, IMapper mapper)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<List<UsuarioDTO>> Lista()
+        {
+            try {
+                var queryUsuario = await _usuarioRepositorio.Consultar();
+                var listaUsuarios= queryUsuario.Include(rol=>rol.IdRolNavigation).ToList();
+                return _mapper.Map<List<UsuarioDTO>>(listaUsuarios);
+            }
+            catch {
+                throw;
+            }
+        }
+
+        public async Task<SesionDTO> ValidarCredenciales(string correo, string clave)
+        {
+            try
+            {
+                var queryUsuario = await _usuarioRepositorio.Consultar(u =>
+                u.Correo == correo &&
+                u.Clave == clave
+                    );
+                if (queryUsuario.FirstOrDefault() == null)
+                    throw new TaskCanceledException("El ususario no existe");
+                Usuario devolverUsuario = queryUsuario.Include(rol => rol.IdRolNavigation).First();
+                return _mapper.Map<SesionDTO>(devolverUsuario);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<UsuarioDTO> Crear(UsuarioDTO modelo)
+        {
+            try
+            {
+                var usuarioCreado = await _usuarioRepositorio.Crear(_mapper.Map<Usuario>(modelo));
+                if(usuarioCreado.IdUsuario==0)
+                    throw new TaskCanceledException("No se pudo crear");
+                var query = await _usuarioRepositorio.Consultar(u => u.IdUsuario == usuarioCreado.IdUsuario);
+                usuarioCreado=query.Include(rol=>rol.IdRolNavigation).First();
+                return _mapper.Map<UsuarioDTO>(usuarioCreado);
+
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Task<bool> Editar(UsuarioDTO modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+    }
+}
